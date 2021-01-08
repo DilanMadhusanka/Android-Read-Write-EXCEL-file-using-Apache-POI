ee3b9f2752123d7e738d07a2c8b05e96136e347c
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2021-01-08 10:21:41 +0530

    read and write txt file

--- AndroidReadWriteEXCELfile/app/src/main/java/com/insharp/android/androidreadwriteexcelfile/MainActivity.java
+++ AndroidReadWriteEXCELfile/app/src/main/java/com/insharp/android/androidreadwriteexcelfile/MainActivity.java
@@ -2,13 +2,141 @@ package com.insharp.android.androidreadwriteexcelfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View writeButton = findViewById(R.id.write);
+        writeButton.setOnClickListener(this);
+        View readButton = findViewById(R.id.read);
+        readButton.setOnClickListener(this);
+        View writeExcelButton = findViewById(R.id.writeExcel);
+        writeExcelButton.setOnClickListener(this);
+        View readExcelButton = findViewById(R.id.readExcel);
+        readExcelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.write:
+                saveFile(this,"myFile.txt");
+                break;
+            case R.id.read:
+                readFile(this,"myFile.txt");
+                break;
+//            case R.id.writeExcel:
+//                saveExcelFile(this,"myExcel.xls");
+//                break;
+//            case R.id.readExcel:
+//                readExcelFile(this,"myExcel.xls");
+//                break;
+        }
+    }
+
+    private static boolean saveFile(Context context, String fileName) {
+
+        // check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.w("FileUtils", "Storage not available or read only");
+            return false;
+        }
+
+        // Create a path where we will place our List of objects on external storage
+        File file = new File(context.getExternalFilesDir(null), fileName);
+        PrintStream p = null; // declare a print stream object
+        boolean success = false;
+
+        try {
+            OutputStream os = new FileOutputStream(file);
+            // Connect print stream to the output stream
+            p = new PrintStream(os);
+            p.println("This is a TEST");
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != p)
+                    p.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return success;
+    }
+
+    private static void readFile(Context context, String filename) {
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+        {
+            Log.w("FileUtils", "Storage not available or read only");
+            return;
+        }
+
+        FileInputStream fis = null;
+
+        try
+        {
+            File file = new File(context.getExternalFilesDir(null), filename);
+            fis = new FileInputStream(file);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                Log.w("FileUtils", "File data: " + strLine);
+                Toast.makeText(context, "File Data: " + strLine , Toast.LENGTH_SHORT).show();
+            }
+            in.close();
+        }
+        catch (Exception ex) {
+            Log.e("FileUtils", "failed to load file", ex);
+        }
+        finally {
+            try {if (null != fis) fis.close();} catch (IOException ex) {}
+        }
+
+        return;
+    }
+
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
     }
 }
